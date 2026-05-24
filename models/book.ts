913dc99fe39561a6acbf9fd596e7404b5b7d50b5
Jen Anderson <andjenm@gmail.com>
2026-05-24 19:53:02 +1200

    basic models in shared folder for reference

---
+++ models/book.ts
@@ -0,0 +1,17 @@
+export interface Book {
+  id: string
+  owner_id: string
+  title: string
+  creator: string //Can include multiple names sep by comma, e.g. Divine comedy translated by Longfellow
+  edition: string //Disambiguate, abridged, collectors etc
+  work_id: string //All LoTR for example; hash for 'orphan' books where ISBN not pulled from dbs
+  isbn?: string // Opt, but preferred. Not all books have. String cuz 0 starts
+  format: string
+  condition?: string
+  search_index?: string
+  lending_terms?: string
+  status: 'Available' | 'On loan' | 'In transit' | 'Reserved'
+  image_urls: string[] //Array of condition images?
+  created_at: string
+  updated_at: string
+}
---
+++ models/loan.ts
@@ -0,0 +1,11 @@
+export interface Loan {
+  id: string
+  book_id: string
+  lender_id: string
+  borrower_id: string
+  status: 'Pending' | 'Active' | 'Completed' | 'Overdue' | 'Declined' | 'Cancelled' //Disable requests if on hold, otherwise not available. Can't deal with queues.
+  due_at: string
+  returned_at: string
+  created_at: string
+  updated_at: string
+}
