59ca17c7624f979ae2344d2228a09d8038c7c19d
Jen Anderson <andjenm@gmail.com>
2026-05-25 22:09:23 +1200

    updated with main, ready to merge hopefully

--- shared/__tests__/calculateDueDate.test.ts
+++ shared/__tests__/calculateDueDate.test.ts
@@ -2,7 +2,7 @@ import { describe , it, expect } from 'vitest'
 import { calculateDueDate } from '../utils/calculateDueDate';
 
 describe('calculates new due date', () => {
-  it ('should correctly add 2 weeksto a standard fixed date', () => {
+  it ('should correctly add 2 weeks to a standard fixed date', () => {
     const fakeToday = '2026-01-10-T12:00:00:000Z'
     const expectedDueDate = '2026-01-24-T12:00:00:000Z'
 
