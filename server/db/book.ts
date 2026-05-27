3eaa7a587828b47fac3378d37c6383144d05be98
Jen Anderson <andjenm@gmail.com>
2026-05-27 22:19:08 +1200

    external search working,very ugly component display

--- server/db/book.ts
+++ server/db/book.ts
@@ -23,3 +23,22 @@ export async function getBooksByOwner(ownerId: string) {
 export async function getBookByTitle(title: string): Promise<Book | undefined> {
   return connection('book').where({ title }).first()
 }
+
+export async function updateBook(id: string, updatedFields: Partial<Book>): Promise<Book> {
+  const bookData = await connection('book')
+  .where({id})
+  .update(updatedFields)
+  .returning('*')
+  return bookData[0] ?? null
+ }
+
+ export async function addBook(newBookData: Omit<Book, 'id'| 'book_id'>): Promise<string>{
+  try {
+    const [insertedId] = await connection('book')
+    .insert(newBookData)
+    return insertedId as unknown as string
+  } catch (err) {
+    console.error('Database insertion error', err)
+    throw err
+  }
+ } 
