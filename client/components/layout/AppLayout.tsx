669f85c20fdc6d7a010d4a49c8e95851acd3fc27
Ezra <ezra@viceversa.co.nz>
2026-06-05 12:37:03 +1200

    fix: now on refresh default user goes abck to being signed in

--- client/components/layout/AppLayout.tsx
+++ client/components/layout/AppLayout.tsx
@@ -1,8 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar' 
-import Footer from './Footer' 
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { useEffect } from 'react'
 
 export default function AppLayout() {
+  useEffect(() => {
+    const stored = localStorage.getItem('active_user_id')
+
+    if (!stored || stored === 'none') {
+      localStorage.setItem('active_user_id', 'u_00001')
+    }
+  }, [])
   return (
     <div className="flex flex-col min-h-screen bg-background text-text">
       <Navbar />
@@ -11,7 +19,7 @@ export default function AppLayout() {
       <main className="flex-1 w-full max-w-app mx-auto px-4 py-6 md:px-6">
         <Outlet />
       </main>
-      
+
       <Footer />
     </div>
   )
